e7434105b0913a07d5659b28855012d242219674
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-14 12:33:45 +0100

    dnoe

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,1 @@
+this is a unittest on functions in the utils file
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,6 +11,8 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    # def test_access_nested_map_exception(self)
+
 
 
 
