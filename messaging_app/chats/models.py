fa8e80ff80c3271e97de98ea3d257e7f677b6e55
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-19 17:54:26 +0100

    add password, firstname and lastname fields

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -9,6 +9,10 @@ class User(AbstactUser):
         default=uuid.uuid4,
         editable=False
     )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=15)
     phone_number = models.CharField(
         max_length=20, 
         null=True, 
