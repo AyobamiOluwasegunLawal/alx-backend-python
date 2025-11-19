354eeba7463253a69e1ccf2f6d375388fd8d211d
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-19 20:39:02 +0100

    add nessted routes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,9 +3,13 @@ from rest_framework import routers
 from .views import MessageViewSet, ConversationViewSet
 
 router = routers.DefaultRouter()
-router.register(r'messages',  MessageViewSet)
-router.register(r'conversations', ConversationViewSet)
+router.register(r'messages',  MessageViewSet, basename='')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls))
+    path('', include(conversations_router.urls))
 ]
