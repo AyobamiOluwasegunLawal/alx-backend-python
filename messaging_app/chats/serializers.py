0030c765ce69077488fcc871bcfdaf974654fb93
AyobamiOluwasegunLawal <ayobamioluwasegslawal@gmail.com>
2025-11-19 19:34:26 +0100

    add serializer methods and validations

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -6,12 +6,30 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = '__all__'
 
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
+    message_body = serializers.CharField(max_length=500)
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+
 class ConversationSerializer(serializers.HyperlinkedModelSerializer):
+    messages = MessageSerializer(many=True, read_only=True, source='message_set')
+    last_messages = serializers.SerializerMethodField()
+
     class Meta:
         model = Conversation
         fields = '__all__'
 
-class MessageSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
+    def get_last_message(self, obj):
+        last_msg = obj.message_set.order_by('-sent_at').first()
+        return last_msg.message_body if last_msg else None
+    
+    def validate(self, data):
+        participants = data.get('participants_id')
+
+        if not participants or len(participants) < 2:
+            raise serializers.ValidationError(
+                "A conversation must have at least 2 participants."
+            )
+        return data
